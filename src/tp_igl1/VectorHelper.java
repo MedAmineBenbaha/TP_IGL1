2012b439b21af48e9f9b68dd0efa42257f0b8bfe
amine <fm_benbaha@esi.dz>
2017-10-23 15:22:42 +0100

    teeeeest conflit

--- src/tp_igl1/VectorHelper.java
+++ src/tp_igl1/VectorHelper.java
@@ -36,7 +36,7 @@ final public class VectorHelper {
      * @param vect2
      * @throws DifferentException 
      */
-    public static void summer(Vector vect1,Vector vect2)throws DifferentException{
+    public static void summerkgfgjggvhcjhfcgfs(Vector vect1,Vector vect2)throws DifferentException{
         if(vect1.size()!=vect2.size()){
             throw new DifferentException();
         }
