6a4f320b920c42d255426703a998698b50955530
amine <fm_benbaha@esi.dz>
2017-10-23 13:57:14 +0100

    first mod

--- src/tp_igl1/Tp_igl1.java
+++ src/tp_igl1/Tp_igl1.java
@@ -5,6 +5,8 @@
  */
 package tp_igl1;
 
+import java.util.Vector;
+
 /**
  *
  * @author mac
@@ -16,6 +18,12 @@ public class Tp_igl1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Vector a=new Vector(3);
+        a.addElement(10);a.addElement(1);a.addElement(5);
+       
+        VectorHelper.vectorSort(a);
+        System.out.println(a.get(0)+"  ++  "+a.get(1)+"  +++++  "+a.get(2));
+      
     }
     
 }
